0e62090b796069a89403e7dc459aa73a932802d3
enamaryn <40593656+enamaryn@users.noreply.github.com>
2023-12-13 15:26:06 +0100

    Update common.sh
    correction 7.0.12 = 7.0

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,4 @@
-export VBOX_VERSION="7.0.12"
+export VBOX_VERSION="7.0"
 export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/7.0.12"
 export VBOX_EXTPACK="Oracle_VM_VirtualBox_Extension_Pack-7.0.12.vbox-extpack"
 export VBOX_GUEST="VBoxGuestAdditions_7.0.12.iso"
