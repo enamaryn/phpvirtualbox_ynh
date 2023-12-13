29d97dc87b65a6ee3a561e15b96b4f613a18e6c5
enamaryn <40593656+enamaryn@users.noreply.github.com>
2023-12-13 15:19:31 +0100

    Update common.sh
    up to 7.0.12

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,5 @@
-export VBOX_VERSION="5.2"
-export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/5.2.6"
-export VBOX_EXTPACK="Oracle_VM_VirtualBox_Extension_Pack-5.2.6-120293.vbox-extpack"
-export VBOX_GUEST="VBoxGuestAdditions_5.2.6.iso"
+export VBOX_VERSION="7.0.12"
+export VBOX_DOWNLOAD="https://download.virtualbox.org/virtualbox/7.0.12"
+export VBOX_EXTPACK="Oracle_VM_VirtualBox_Extension_Pack-7.0.12.vbox-extpack"
+export VBOX_GUEST="VBoxGuestAdditions_7.0.12.iso"
 
